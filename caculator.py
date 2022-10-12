7f5d05dba4633ead44662c14a694db990223429a
Solomon <sshiferaw@faculty.ie.edu>
2022-10-12 16:54:41 +0200

    comment multiplication

--- caculator.py
+++ caculator.py
@@ -9,8 +9,8 @@ def subtract(x, y):
     return x - y
 
 # This function multiplies two numbers
-def multiply(x, y):
-    return x * y
+# def multiply(x, y):
+#     return x * y
 
 # This function divides two numbers
 # def divide(x, y):
@@ -20,8 +20,8 @@ def multiply(x, y):
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+# print("3.Multiply")
+# print("4.Divide")
 
 while True:
     # take input from the user
@@ -38,8 +38,8 @@ while True:
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+        # elif choice == '3':
+        #     print(num1, "*", num2, "=", multiply(num1, num2))
 
         # elif choice == '4':
         #     print(num1, "/", num2, "=", divide(num1, num2))
